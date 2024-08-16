f38f176496e888dfcc9ff8aae9f4a1b5ad74989c
leonardoIFBA <leonardorodrigues@ifba.edu.br>
2024-08-16 16:05:43 -0300

    Criando o repo

---
+++ src/main/java/com/example/ifbademo/model/Departamento.java
@@ -0,0 +1,21 @@
+package com.example.ifbademo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends AbstractEntity<Long>{
+    @Column(nullable = false, unique = true, length = 60)
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    
+}
